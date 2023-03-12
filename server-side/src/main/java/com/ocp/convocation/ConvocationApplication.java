ed6b822c14067c54c48bb9c635de6cd2e49f4eef
OTHmanera <raisothmane4@gmail.com>
2023-03-12 01:58:59 +0100

    Convocation-project

---
+++ server-side/src/main/java/com/ocp/convocation/ConvocationApplication.java
@@ -0,0 +1,14 @@
+package com.ocp.convocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvocationApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ConvocationApplication.class, args);
+	}
+
+}
